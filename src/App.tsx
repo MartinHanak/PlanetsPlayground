16f121260ed68a8d699c3dc28cc35ec8803d35bf
Martin Hanák <martinhanak97@gmail.com>
2023-02-02 16:10:06 +0100

    Add bare pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
 import './assets/App.css';
 
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
 import Home from './pages/Home';
+import Import from './pages/Import';
+import Simulation from './pages/Simulation';
+import Contact from './pages/Contact';
 import About from './pages/About';
 import NotFound from './pages/NotFound';
+
 
 import { Route, Routes } from 'react-router-dom';
 
@@ -19,6 +22,9 @@ function App() {
       <main>
         <Routes>
           <Route path="/" element={<Home />}></Route>
+          <Route path="/import" element={<Import />}></Route>
+          <Route path="/simulation" element={<Simulation />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
           <Route path="/about" element={<About />}></Route>
           <Route path="*" element={<NotFound />}></Route>
         </Routes>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Navbar() {
@@ -6,6 +5,9 @@ export default function Navbar() {
         <nav>
             <ul>
                 <li><Link to="/">Home</Link></li>
+                <li><Link to="/import">Import</Link></li>
+                <li><Link to="/simulation">Simulation</Link></li>
+                <li><Link to="/contact">Contact</Link></li>
                 <li><Link to="/about">About</Link></li>
             </ul>
         </nav>
