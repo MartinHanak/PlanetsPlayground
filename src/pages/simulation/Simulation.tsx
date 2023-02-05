bcae4a3af3a26526f684fdd4066a77bbba9c24af
Martin Hanák <martinhanak97@gmail.com>
2023-02-05 20:59:55 +0100

    Fix esling settings for React Three Fiber unknown properties

--- src/pages/simulation/Simulation.tsx
+++ src/pages/simulation/Simulation.tsx
@@ -33,7 +33,6 @@ export default function Simulation() {
             <h1>Simulation</h1>
             <Canvas>
                 <ambientLight />
-                <pointLight position={[10, 10, 10]} />
                 <Box position={[-1.2, 0, 0]} />
                 <Box position={[1.2, 0, 0]} />
             </Canvas>
